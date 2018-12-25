cdf0b46ccbe359974224771db55fc0af1276e0f5
Wesley Hitson <wesleyhitson@users.noreply.github.com>
2018-12-24 23:14:20 -0500

    Rudimentary number extraction

--- numberExtract.js
+++ numberExtract.js
@@ -1,2 +1,17 @@
-console.log("Looking for phone numbers. - Craigslist Number Extractor")
+console.log("Looking for phone numbers. - Craigslist Number Extractor");
 
+function findNumber(text) {
+    var words = text.split(" ");
+    for (i = 0; i < words.length; i++) {
+        if (words[i].includes("9")) {
+            console.log(words[i]);
+        }
+    }
+}
+
+function getBody() {
+    var body = document.getElementById("postingbody").innerHTML;
+    findNumber(body);
+}
+
+getBody()
